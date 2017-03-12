76b305e1548c4d86b25324c2d782708c69036ddb
Miłosz Szyrner <milosz.szyrner@gmail.com>
2017-03-12 23:49:44 +0100

    Init

---
+++ Lab2/Lab2/Game1.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.IO;
+
+namespace Lab2
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Sprite dragonBallHero1;
+        Sprite dragonBallHero;
+        private SpriteFont font;
+        private bool isCollision = false;
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            //Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            Texture2D sample = Texture2D.FromStream(GraphicsDevice, File.OpenRead("Content/dragonball.png"));
+            dragonBallHero1 = new Sprite(sample, new Vector2(500, 100));
+            dragonBallHero = new Sprite(sample, new Vector2(50, 50));
+            font = Content.Load<SpriteFont>("Content/Tekst");
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            var keyboardState = Keyboard.GetState();
+
+            if(keyboardState.IsKeyDown(Keys.W))
+            {
+                dragonBallHero.position.Y -= 100 * gameTime.ElapsedGameTime.Milliseconds / 1000F;
+            }
+            if (keyboardState.IsKeyDown(Keys.S))
+            {
+                dragonBallHero.position.Y += 100 * gameTime.ElapsedGameTime.Milliseconds / 1000F;
+            }
+            if (keyboardState.IsKeyDown(Keys.A))
+            {
+                dragonBallHero.position.X -= 100 * gameTime.ElapsedGameTime.Milliseconds / 1000F;
+            }
+            if (keyboardState.IsKeyDown(Keys.D))
+            {
+                dragonBallHero.position.X += 100 * gameTime.ElapsedGameTime.Milliseconds / 1000F;
+            }
+            dragonBallHero.Update(gameTime);
+            dragonBallHero1.Update(gameTime);
+
+            isCollision = false;
+            if (dragonBallHero.boundingBox.Contains(dragonBallHero1.boundingBox) == ContainmentType.Intersects || dragonBallHero.boundingSphere.Contains(dragonBallHero1.boundingSphere) == ContainmentType.Intersects)
+            {
+                isCollision = true;
+            }
+
+                base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, SamplerState.PointWrap);
+            spriteBatch.DrawString(font, (isCollision == true) ? "We stick together" : "We are apart", new Vector2(100, 20), Color.Black);
+            dragonBallHero1.Draw(spriteBatch);
+            dragonBallHero.Draw(spriteBatch);
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Lab2/Lab2/Sprite.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Sprite
+    {
+        public BoundingBox boundingBox { get; set; }
+        public Vector2 position;
+        public Texture2D texture { get; }
+        public BoundingSphere boundingSphere { get; set; }
+        public void setPosition(float x, float y)
+        {
+            position = new Vector2(x, y);
+        }
+        public Sprite(Texture2D texture, Vector2 position)
+        {
+            this.texture = texture;
+            this.position = position;
+        }
+        public Sprite(Texture2D texture)
+        {
+            this.texture = texture;
+            this.position = position;
+        }
+
+        private void UpdateBoundingBox()
+        {
+            boundingBox = new BoundingBox(new Vector3(position, 0), new Vector3(position.X + texture.Width, position.Y + texture.Height, 0));
+        }
+        private void UpdateBoundingSphere()
+        {
+            //boundingSphere = new BoundingSphere(new Vector3(position.X + (texture.Width / 2), position.Y + (texture.Height / 2), 0), (float)Math.Sqrt(Math.Pow((position.X + texture.Width) - (position.X + (texture.Width / 2)), 2) + Math.Pow((position.Y + texture.Height) - (position.Y + (texture.Height / 2)), 2)));
+            boundingSphere = new BoundingSphere(new Vector3(position.X + (texture.Width / 2), position.Y + (texture.Height / 2), 0), Math.Max(texture.Height / 2, texture.Width / 2));
+        }
+        public void Update(GameTime pGameTime)
+        {
+            UpdateBoundingBox();
+            UpdateBoundingSphere();
+        }
+        public void Draw(SpriteBatch sp)
+        {
+            sp.Draw(texture, position, null, Color.White);
+        }
+    }
+}
