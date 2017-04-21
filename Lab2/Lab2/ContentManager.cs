01a06147a7b8a71c5d006ae93215a660ddc86802
miloszszyrner <milosz.szyrner@gmail.com>
2017-04-21 11:35:42 +0200

    Add ContentManager

---
+++ Lab2/Lab2/ContentManager.cs
@@ -0,0 +1,20 @@
+﻿using Lab2;
+using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace ToA
+{
+    public class ContentManager
+    {
+        ContentManager content;
+        List<Sprite> spriteList;
+        XDocument xDoc;
+        public ContentManager(String filename, ContentManager content)
+        {
+            xDoc = XDocument.Load(filename);
+            this.content = content;
+        }
+        
+    }
+}
