e5fde0b9743f80c28035a01d4f4fab6585d28bdf
Michal Kaminski <michalkam12@gmail.com>
2017-07-04 23:55:16 +0200

    better jump animation

--- Lab2/Lab2/Player.cs
+++ Lab2/Lab2/Player.cs
@@ -31,7 +31,6 @@ namespace ToA
                 position.Y -= 10f;
                 velocity.Y = -12f;
                 hasJumped = true;
-                Animate(pGameTime, 2);
                 Game1.Instance.SoundManager.Sounds["Jump"].Play(Game1.Instance.spriteEffectVolume, 0f, 0f);
             }
             if (Game1.Instance.InputManager.up && !gravity && isGravity)
@@ -39,7 +38,6 @@ namespace ToA
                 position.Y += 10f;
                 velocity.Y = 12f;
                 isGravity = false;
-                Animate(pGameTime, 3);
                 Game1.Instance.SoundManager.Sounds["Jump"].Play(Game1.Instance.spriteEffectVolume, 0f, 0f);
             }
             if (Game1.Instance.InputManager.right && !hasJumped)
@@ -54,6 +52,10 @@ namespace ToA
             }
             else if(!hasJumped)
                 sourceRectangle = new Rectangle(0, 0, animationX, animationY);
+            if(hasJumped)
+            {
+                Animate(pGameTime, 2);
+            }
             if (Game1.Instance.InputManager.right == Game1.Instance.InputManager.left)
                 velocity.X = 0f;
 
